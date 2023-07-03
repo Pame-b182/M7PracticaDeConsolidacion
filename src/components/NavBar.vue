08db5ae6563b7319003dd16da94e7fb8b97a5b62
pamecorreabarrera@gmail.com <pamecorreabarrera@gmail.com>
2023-07-03 19:43:32 -0400

    Commit: Se crea el proyecto y se cumplen todos los requerimientos

---
+++ src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <v-app-bar
+      color="deep-purple accent-4"
+      dense
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Cursos AlfaWeb</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn to="/" icon>
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+
+      <v-btn to="/admin" icon>
+        <v-icon color="white">mdi-wrench</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'navbar-component',
+    // props: {},
+    data: function(){
+        return {}
+    },
+    // computed: {},
+    //methods: {}
+    // watch: {},
+    // components: {},
+    // mixins: [],
+    // filters: {},
+    // -- Lifecycle Methods
+    // -- End Lifecycle Methods
+}
+</script>
+
+<style scoped>
+    
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,77 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <h1 class="text-center">Lista de Cursos</h1>
+    <v-row justify="space-around">
+      <v-card v-for="curso in cursos" :key="curso.id" class="mt-8 mx-1" width="290">
+        <v-img contain height="200px" :src="curso.img">
+          <v-app-bar flat color="rgba(0, 0, 0, 0)"> </v-app-bar>
+        </v-img>
+        <h4 class="text-center mt-5">{{ curso.nombre }}</h4>
+        <v-card-text>
+          <div class="font-weight-bold ml-8 mb-2">Información</div>
+
+          <v-timeline align-top dense>
+            <v-timeline-item small color="deep-purple lighten-1">
+              <div>
+                <div class="font-weight-normal">
+                  Costo: <strong>${{ curso.costo }}</strong>
+                </div>
+                <div>Duración: {{ curso.duracion }}</div>
+              </div>
+            </v-timeline-item>
+            <v-timeline-item small color="green">
+              <div>
+                <div class="font-weight-normal">
+                  Cupos: <strong>{{ curso.cupos }}</strong>
+                </div>
+                <div>Completado: {{ cursoTerminado(curso.id) }}</div>
+              </div>
+            </v-timeline-item>
+            <v-timeline-item small color="deep-purple lighten-1">
+              <div>
+                <div class="font-weight-normal">
+                  Fecha de Registro:
+                </div>
+                <div>{{ curso.fecha_registro }}</div>
+              </div>
+            </v-timeline-item>
+             <v-timeline-item small color="green">
+              <div>
+                <div class="font-weight-normal">
+                  Descripción
+                </div>
+                <div>{{ curso.descripcion}}</div>
+              </div>
+            </v-timeline-item>
+          </v-timeline>
+        </v-card-text>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+<script>
+import { mapState, mapGetters } from "vuex";
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home-view",
+  // props: {},
+  data: function () {
+    return {};
+  },
+  computed: {
+    ...mapState(["cursos"]),
+    ...mapGetters(["cursoTerminado"]),
+  },
+  //methods: {}
+  // watch: {},
+  // components: {},
+  // mixins: [],
+  // filters: {},
+  // -- Lifecycle Methods
+  // -- End Lifecycle Methods
+};
 </script>
+
+<style scoped>
+</style>
